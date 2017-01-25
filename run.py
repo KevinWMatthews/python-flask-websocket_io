c31998ce9e3b6eb5aded1cb97c651646a4a40d9a
Kevin Matthews <kmatthews@cyberdata.net>
2017-01-25 11:30:11 -0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,3 @@
+from websocket_io import app
+import websocket_io.views
+app.run(host'127.0.0.1', port=5000)
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'websocket_io',
+    packages = ['websocket_io'],
+    include_package_data = True,
+    install_requries = [
+        'flask',
+    ],
+)
